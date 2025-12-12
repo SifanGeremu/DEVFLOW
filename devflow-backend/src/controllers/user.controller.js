6c1796a0823be165e27a9fb65147e29d1b8a1e4b
SifanGeremu <wins33975@gmail.com>
2025-12-12 21:48:37 +0300

    making progress on user profile

--- devflow-backend/src/controllers/user.controller.js
+++ devflow-backend/src/controllers/user.controller.js
@@ -4,6 +4,19 @@ const userProfile = async(req,res)=>{
         const name = req.name;
         
     } catch (error) {
+        console.log(error)
+    }
+}
+
+const updateUserProfile = async (req,res)=>{
+    try {
+        const userId = req.userId;
+        const {name} = req.body;
+
+        if(!name){
+            return res.status(400).json({message:"Name is required"});
         
+    } catch (error) {
+        console.log(err)
     }
 }
