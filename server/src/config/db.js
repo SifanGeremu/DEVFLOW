9a64a614ec89d2815e5cb1d4d0349b52eab8eba8
SifanGeremu <wins33975@gmail.com>
2026-01-24 22:17:12 +0300

    Move db config to src/config and use env variable for Aiven password

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,26 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-const pool =mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-})
 
-  async function checkConnection() {
-    try {
-      const connection = await pool.getConnection();
-      console.log("Connected to MySQL database!");
-      connection.release(); // release back to pool
-    } catch (err) {
-      console.error(" Database connection failed:", err.message);
-    }
-
-  }
 
 checkConnection();
 export default pool;
